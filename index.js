be6b9502f7c9cb842ddb2f45b3114531ee21d987
Jyotiranjan Patra <112277029+Jrpatra706@users.noreply.github.com>
2023-02-09 19:43:17 +0530

    commit initial files

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('hello');
+let a=6;
+console.log(a);
+//dynamic typing
+let name='jyoti';
+console.log(name);
+name=8;
+console.log(name);
+//array
+let names =['ram', 'jp'];
+console.log(names[2]);
